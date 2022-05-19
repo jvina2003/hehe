853911c5a8afe84c723defad10fbf41f60a6b7b3
jvina2003 <jvina2003@outlook.com>
2022-05-19 17:00:51 -0500

    1

---
+++ main.js
@@ -0,0 +1,43 @@
+function bodyPaint() {
+	window.addEventListener("scroll", function () {
+		const scrollTop =
+			window.pageYOffset ||
+			document.documentElement.scrollTop ||
+			document.body.scrollTop ||
+			0;
+
+		const triggers = document.querySelectorAll("[data-color]");
+
+		const body = document.body;
+
+		// Change 33% earlier than scroll position so colour is there when you arrive.
+		const scroll = scrollTop + window.innerHeight / 3;
+
+		for (var i = 0; i < triggers.length; i++) {
+			const trigger = triggers[i];
+			const triggerRect = trigger.getBoundingClientRect();
+			const triggerRectTop = triggerRect.top + scrollTop;
+			const triggerHeight = trigger.offsetHeight;
+			const triggerColor = trigger.getAttribute("data-color");
+
+			// if position is within range of this panel.
+			// So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+			// Remember we set the scroll to 33% earlier in scroll var.
+			if (triggerRectTop <= scroll && triggerRectTop + triggerHeight > scroll) {
+				// Set attribute of currently active div
+				body.setAttribute("data-paint", triggerColor);
+				console.log(body.dataset.paint);
+			}
+		}
+	});
+}
+
+bodyPaint();
+
+function toggleDarkLight() {
+	var body = document.getElementById("body");
+	var currentClass = body.className;
+	body.className = currentClass == "light-mode" ? "dark-mode" : "light-mode";
+  }
+
+  
