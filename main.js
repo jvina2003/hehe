d60ef789e6497cc14fc545713082a97fd2ba4d2f
jvina2003 <jvina2003@outlook.com>
2022-07-03 18:03:15 +0700

    VERSION 2 BIG CHANGE

--- main.js
+++ main.js
@@ -170,3 +170,54 @@ function topFunction() {
 
 
 
+
+
+$(function(){
+	$('.carousel-item').eq(0).addClass('active');
+	var total = $('.carousel-item').length;
+	var current = 0;
+	$('#moveRight').on('click', function(){
+	  var next=current;
+	  current= current+1;
+	  setSlide(next, current);
+	});
+	$('#moveLeft').on('click', function(){
+	  var prev=current;
+	  current = current- 1;
+	  setSlide(prev, current);
+	});
+	function setSlide(prev, next){
+	  var slide= current;
+	  if(next>total-1){
+	   slide=0;
+		current=0;
+	  }
+	  if(next<0){
+		slide=total - 1;
+		current=total - 1;
+	  }
+			 $('.carousel-item').eq(prev).removeClass('active');
+			 $('.carousel-item').eq(slide).addClass('active');
+		setTimeout(function(){
+  
+		},800);
+	  
+  
+	  
+	  console.log('current '+current);
+	  console.log('prev '+prev);
+	}
+  });
+
+
+ // const imgContent = document.querySelectorAll('.img-content-hover');
+
+//function showImgContent(e) {
+ // for(var i = 0; i < imgContent.length; i++) {
+  //  x = e.pageX;
+  //  y = e.pageY;
+  //  imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
+ // }
+//};
+
+//document.addEventListener('mousemove', showImgContent);
